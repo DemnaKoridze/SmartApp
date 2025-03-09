1d3ae75340983e7c7e692563140b4d346b7000e0
Demna Koridze <demnaqoridze@gmail.com>
2025-03-09 13:21:05 +0400

    first

---
+++ SmartApp/SmartApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartApp
+//
+//  Created by demnakoridze on 09.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
